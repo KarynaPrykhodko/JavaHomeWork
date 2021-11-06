2f47b7b0471bc617b3a9a50d8ad49590f305a54a
Karyna Prykhodko <karyna.prykhodko@privatbank.ua>
2021-11-06 23:46:11 +0200

    init commit

---
+++ src/com/pb/prykhodko/hw6/Animal.java
@@ -0,0 +1,63 @@
+package com.pb.prykhodko.hw6;
+
+import java.util.Objects;
+
+public class Animal {
+    private String food;
+    private String location;
+
+    public Animal(String food, String location) {
+        this.food = food;
+        this.location = location;
+    }
+
+    public void makeNoise() {
+        System.out.println("noise");
+    }
+
+    public void eat() {
+        System.out.println("i am eating");
+    }
+
+    public void sleep() {
+        System.out.println("The animal sleep");
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "food='" + food + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(food, animal.food) &&
+                Objects.equals(location, animal.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(food, location);
+    }
+}
