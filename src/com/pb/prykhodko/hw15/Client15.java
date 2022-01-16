599331f2da54dff19c20c131af5877837f72a95f
Karyna Prykhodko <karyna.prykhodko@privatbank.ua>
2022-01-16 22:09:18 +0200

    init commit

---
+++ src/com/pb/prykhodko/hw15/Client15.java
@@ -0,0 +1,99 @@
+package com.pb.prykhodko.hw15;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Client15 extends JFrame {
+
+    String serverIp = "127.0.0.1";
+    int serverPort = 1236;
+    private Socket client15Socket;
+    private Scanner inMessage;
+    private PrintWriter outMessage;
+    private JTextField jtfMessage;
+    private JTextArea jtaTextAreaMessage;
+
+    public Client15() {
+        try {
+            client15Socket = new Socket(serverIp, serverPort);
+            inMessage = new Scanner(client15Socket.getInputStream());
+            outMessage = new PrintWriter(client15Socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setBounds(600, 300, 600, 500);
+        setTitle("Client15");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jtaTextAreaMessage = new JTextArea();
+        jtaTextAreaMessage.setEditable(false);
+        jtaTextAreaMessage.setLineWrap(true);
+        JScrollPane jsp = new JScrollPane(jtaTextAreaMessage);
+        add(jsp, BorderLayout.CENTER);
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        add(bottomPanel, BorderLayout.SOUTH);
+        JButton jbSendMessage = new JButton("Отправить");
+        bottomPanel.add(jbSendMessage, BorderLayout.EAST);
+        jtfMessage = new JTextField("Введите ваше сообщение: ");
+        bottomPanel.add(jtfMessage, BorderLayout.CENTER);
+        jbSendMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!jtfMessage.getText().trim().isEmpty()) {
+                    sendMsg();
+                    jtfMessage.grabFocus();
+                }
+            }
+        });
+        jtfMessage.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                jtfMessage.setText("");
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        if (inMessage.hasNext()) {
+                            String inMes = inMessage.nextLine();
+                                jtaTextAreaMessage.append(inMes);
+                                jtaTextAreaMessage.append("\n");
+                        }
+                    }
+                } catch (Exception e) {
+                }
+            }
+        }).start();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try {
+                    outMessage.close();
+                    inMessage.close();
+                    client15Socket.close();
+                } catch (IOException exc) {
+
+                }
+            }
+        });
+        setVisible(true);
+    }
+    public void sendMsg() {
+        String messageStr = jtfMessage.getText();
+        outMessage.println(messageStr);
+        outMessage.flush();
+        jtfMessage.setText("");
+    }
+
+    public static void main(String[] args) {
+        Client15 client15 = new Client15();
+    }
+}
